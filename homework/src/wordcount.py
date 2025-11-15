c432e7b029b7b1a28afcc08bee6c7ec8cf1444fc
sgutierrezos04 <sgutierrezos@unal.edu.co>
2025-11-15 12:34:30 -0500

    1er parte

---
+++ homework/src/wordcount.py
@@ -0,0 +1,46 @@
+# obtain a list of files in the input directory
+import os
+
+
+def read_all_lines():
+    all_lines = []
+    input_files_list = os.listdir("data/input/")
+
+    for filename in input_files_list:
+        with open("data/input/" + filename, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+    return all_lines
+
+
+def main():
+
+    input_files_list = os.listdir("data/input/")
+
+    # count the frequency of the words in the files in the input directory
+    counter = {}
+    for filename in input_files_list:
+        with open("data/input/" + filename) as f:
+            for l in f:
+                for w in l.split():
+                    w = w.lower().strip(",.!?")
+                    counter[w] = counter.get(w, 0) + 1
+
+    # create the directory output/ if it doesn't exist
+    write_count_words(counter)
+
+
+def write_count_words(counter):
+    if not os.path.exists("data/output"):
+        os.makedirs("data/output")
+
+    # save the results using tsv format
+    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
+        for key, value in counter.items():
+            # write the key and value to the file
+            f.write(f"{key}\t{value}\n")
+
+
+if __name__ == "__main__":
+
+    main()
